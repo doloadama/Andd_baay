42d6383252a04e6c4469c68c9e91c1629a7c0244
assy <adjietou98@gmail.com>
2024-12-06 16:40:27 +0000

    test

--- baay/views.py
+++ baay/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 import streamlit as st
-from baay.models import FruitLegume  # Importer le modèle Django
+
 
 # Liste statique des fruits et légumes cultivés au Sénégal
 fruits_et_legumes_senegal = [
