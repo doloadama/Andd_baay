41081d9f749b1e632d2ab63d2dce92d2c3f879f4
doloadama <doloadama@outlook.com>
2025-04-14 07:51:14 +0000

    finished

--- Andd_Baayi/settings.py
+++ Andd_Baayi/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'Andd_Baayi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BAYY',
+        'USER': 'Dolo',
+        'PASSWORD': 'toto',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
