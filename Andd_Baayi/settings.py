b5d3582a3060380d815ed4947f2399df38dfe519
adamadolo <doloadama@outlook.com>
2025-04-14 10:30:11 +0000

    .

--- Andd_Baayi/settings.py
+++ Andd_Baayi/settings.py
@@ -17,8 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
