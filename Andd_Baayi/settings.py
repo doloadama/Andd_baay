a24f1ecb2409fe16785231d59cda25bba062e314
adamadolo <doloadama@outlook.com>
2025-04-14 10:29:36 +0000

    CSRF authorization

--- Andd_Baayi/settings.py
+++ Andd_Baayi/settings.py
@@ -26,7 +26,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 SECRET_KEY = 'django-insecure-51*3o!q=pag5959-3!glvv_v##c-pbk!sj+w0sc()=2ci9&6*7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['andd-baay.onrender.com']
 
