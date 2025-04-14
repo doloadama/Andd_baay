4768f2be90e0f97d58a0a6fdf87277443be0cec0
adamadolo <doloadama@outlook.com>
2025-04-14 16:03:22 +0000

    /

--- Andd_Baayi/settings.py
+++ Andd_Baayi/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'baay/static',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
