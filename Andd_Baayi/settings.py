9f7212188c64822705b4fa9de5f0cc99ecad6bf9
adamadolo <doloadama@outlook.com>
2025-04-14 20:00:21 +0000

    .

--- Andd_Baayi/settings.py
+++ Andd_Baayi/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = 'baay/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'baay/static',
