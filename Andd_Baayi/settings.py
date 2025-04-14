e32a0136a088d4f011edd49525e821819c88897e
adamadolo <doloadama@outlook.com>
2025-04-14 09:36:51 +0000

    host added

--- Andd_Baayi/settings.py
+++ Andd_Baayi/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-51*3o!q=pag5959-3!glvv_v##c-pbk!sj+w0sc()=2ci9&6*7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['andd-baay.onrender.com']
 
 
 INSTALLED_APPS = [
